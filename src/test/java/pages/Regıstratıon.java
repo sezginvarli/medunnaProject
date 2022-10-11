dd661962b4d09226176e981980a680a6588d17c0
hkilicaslan-sdet <hkilicaslansdet@gmail.com>
2022-10-11 20:30:37 +0300

    update.2

---
+++ src/test/java/pages/Regıstratıon.java
@@ -0,0 +1,19 @@
+package pages;
+
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class Regıstratıon {
+
+    public Regıstratıon (){
+        PageFactory.initElements(Driver.getDriver(),this);
+
+    }
+
+    //ssn, fırstname and lastname
+
+    //username, emaıl
+
+
+
+}
