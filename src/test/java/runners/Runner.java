d52eda9d02a262c1d540ad391af268066fbd6766
edaonat <107421060+edaonat@users.noreply.github.com>
2022-10-18 09:50:14 +0300

    Update Runner.java

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -12,6 +12,7 @@ import org.junit.runner.RunWith;
                 "rerun:target/failedRerun.txt"
         },
         features = "./src/test/resources/features",
+        glue = {"stepdefinitions","hooks"},
         tags = "",
         dryRun = false
 )
