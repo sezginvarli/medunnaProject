791bf34bca339fc49f69c6e675ca4d3741d0bd09
hkilicaslan-sdet <hkilicaslansdet@gmail.com>
2022-10-21 22:31:19 +0300

    update hk 22:30

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         },
         features = "./src/test/resources/features",
         glue = {"stepdefinitions","hooks"},
-        tags = "@US_008",
+        tags = "@PhysicianAppointmentEdit",
         dryRun = false
 )
 public class Runner {
