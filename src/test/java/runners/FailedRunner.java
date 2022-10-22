b952151d88de87a60c067d5d4e739d18eec8c988
Orhan Ayyıldız <ayyildizorhann03@gmail.com>
2022-10-22 17:13:39 +0300

    failed runner

--- src/test/java/runners/FailedRunner.java
+++ src/test/java/runners/FailedRunner.java
@@ -1,21 +1,22 @@
 package runners;
+
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
+
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        monochrome = true,
         plugin = {
                 "pretty",
-                "html:target/default-cucumber-reports.html",
+                "html:target/html-reports/cucumber.html",
                 "json:target/json-reports/cucumber.json",
                 "junit:target/xml-report/cucumber.xml",
                 "rerun:target/failedRerun.txt"
         },
-        features = "./src/test/resources/features",
-        glue = {"stepdefinitions","hooks"},
-        tags = "@Signinn",
+        features = "@target/failedRerun.txt",
+        glue = {"stepdefinitions"},
         dryRun = false
 )
-public class Runner {
+public class FailedRunner {
 }
-//@US_008
