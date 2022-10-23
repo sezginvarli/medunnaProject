cb84bf445830dce8caba39a58319ece1a591e647
HusrevAkbas <husrev.akbas@gmail.com>
2022-10-23 13:15:53 +0200

    chrome language option added

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         },
         features = "./src/test/resources/features",
         glue = {"stepdefinitions","hooks"},
-        tags = "@US07-14",//
+        tags = "@",//
         dryRun =false
 )
 public class Runner {
