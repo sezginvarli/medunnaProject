c87c2174cd1858f3553bed10e8dc4e14cd494694
Şeyma <seyma.kznc@gmail.com>
2022-10-23 09:25:00 +0300

    seyma

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         },
         features = "./src/test/resources/features",
         glue = {"stepdefinitions","hooks"},
-        tags = "@Signinn",
+        tags = "@US_0004",
         dryRun = false
 )
 public class Runner {
