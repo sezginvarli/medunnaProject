ada0f1d3bc0c71f56a377cd5b4e9e46552e6c41a
Orhan Ayyıldız <ayyildizorhann03@gmail.com>
2022-10-31 10:55:03 +0300

    last

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         },
         features = "./src/test/resources/features",
         glue = {"stepdefinitions","hooks"},
-        tags = "@Signinn",
+        tags = "@allPhysicians",
         dryRun = false
 )
 public class Runner {
